816bc7b7e350e3a5f65535099c3e6afe287c2d51
Ryan Beam <59149378+beam42@users.noreply.github.com>
2022-12-03 15:40:29 -0800

    Create Pacman.cpp

---
+++ Application/Scene/Pacman.cpp
@@ -0,0 +1,56 @@
+#include "Pacman.h"
+
+#undef M_PI
+
+namespace Nome::Scene
+{
+
+DEFINE_META_OBJECT(CCircle)
+{
+    BindPositionalArgument(&CCircle::Radius, 1, 0);
+    BindPositionalArgument(&CCircle::Segments, 1, 1);
+}
+
+void CCircle::MarkDirty()
+{
+    // Mark this entity dirty
+    Super::MarkDirty();
+
+    // And also mark the Face output dirty
+    Circle.MarkDirty();
+}
+
+void CCircle::UpdateEntity()
+{
+    if (!IsDirty())
+        return;
+
+    Super::UpdateEntity();
+    int n = (int)Segments.GetValue(6.0f);
+    float radius = Radius.GetValue(1.0f);
+
+    std::vector<Vertex*> handles;
+    std::vector<CVertexInfo *> positions;
+    for (int i = 0; i < n; i++)
+    {
+        float theta = (float)i / n * 2.f * (float)tc::M_PI;
+        Vector3 pos = Vector3(radius * cosf(theta+maw), radius * sinf(theta+maw), 0.0f);
+        handles.push_back(AddVertex("v" + std::to_string(i), pos));
+        CVertexInfo *point = new CVertexInfo();
+        point->Position = pos;
+        positions.push_back(point);
+    }
+    positions.push_back(positions[0]);
+    handles.push_back(handles[0]);
+
+    AddLineStrip("circle", handles);
+
+    // Sweep path info
+    SI.Positions = positions;
+    SI.IsClosed = true;
+    SI.Name = GetName();
+    Circle.UpdateValue(&SI);
+    SetValid(true);
+}
+
+}
