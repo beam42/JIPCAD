d830b0be865e25d711d8dc9f9a18978d339f5610
Ryan Beam <59149378+beam42@users.noreply.github.com>
2022-12-03 15:10:33 -0800

    Create Pacman.h
    defined "Maw" input.
    Otherwise, identical to Circle.h-- maybe too identical? Possible some other changes are necessary.

---
+++ Application/Scene/Pacman.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Mesh.h"
+#include "SweepPath.h"
+
+namespace Nome::Scene
+{
+
+class CCircle : public CSweepPath
+{
+    DEFINE_INPUT(float, Segments) { MarkDirty(); }
+    DEFINE_INPUT(float, Radius) { MarkDirty(); }
+    DEFINE_INPUT(float, Maw ) { MarkDirty(); }
+    DEFINE_OUTPUT_WITH_UPDATE(CSweepPathInfo*, Circle) { UpdateEntity(); }
+
+public:
+    DECLARE_META_CLASS(CCircle, CSweepPath);
+    CCircle() = default;
+    CCircle(const std::string& name)
+        : CSweepPath(name)
+    {
+    }
+
+    void UpdateEntity() override;
+    void MarkDirty() override;
+
+};
+
+}
